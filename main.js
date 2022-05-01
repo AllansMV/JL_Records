a67981d779b75889cafbaa85340309758a1ce00c
AllansMV <amejiavelasquez@gmail.com>
2022-04-30 20:58:15 -0600

    version 1.4

---
+++ main.js
@@ -0,0 +1,15 @@
+function facebook(){
+    location.href="https://www.facebook.com/AVI-323141422433002"
+}
+
+function whatsapp(){
+    location.href="https://api.whatsapp.com/send/?phone=50256104269&text=%C2%A1Buen+d%C3%ADa%2C+AVI%21+Deseo+m%C3%A1s+informaci%C3%B3n+de+los+paquetes%2C+por+favor%21&app_absent=0"
+}
+
+function email(){
+    location.href="mailto:amejiavelasquez@gmail.com"
+}
+
+function instagram(){
+    location.href="https://www.instagram.com/avi_estudio/"
+}
